c9b8d1ce00d9c428e1d96d7fa675afd8791b8abc
zokasta <125266300+zokasta@users.noreply.github.com>
2024-06-24 13:57:11 +0530

    react router dom has been added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'sm': {'max': '567px'},
+      'md': {'min': '568px', 'max': '823px'},
+      'lg': {'min': '824px', 'max': '1079px'},
+      'xl': {'min': '1280px'},
+    },  
   },
   plugins: [],
 }

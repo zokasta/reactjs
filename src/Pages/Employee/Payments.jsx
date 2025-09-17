7db35daff0c9fb16e477028d61f136e0a0ffabfe
zokasta <faizrajput1510@outlook.com>
2025-09-17 14:31:10 +0530

    for solving error

--- src/Pages/Employee/Payments.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function Payments(){
-    return(
-        <div></div>
-    )
-}
--- src/Pages/Employee/Tickets.jsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function Tickets(){
-    return(
-        <div></div>
-    )
-}
